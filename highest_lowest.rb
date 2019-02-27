5ed1db991f405421718b0b503de79c5a2d0ddfbe
Harry Riley <HarryRiley@applemacs-mbp.home>
2019-02-27 20:57:51 +0000

    Convert list to integers

--- highest_lowest.rb
+++ highest_lowest.rb
@@ -1,9 +1,14 @@
 def high_and_low(numbers_string)
-  string_output = numbers_string.split.(" ")
-  print string_output
+  string_output = numbers_string.split(" ")
+  int_array = []
+  string_output.each { |element|
+  int_array << element.to_i }
+  print int_array
 end
 
 
-test = "10 -9 -2 432 942 8 51"
+test = "10 13 14 65 40"
 
 high_and_low(test)
+
+# -9 -2 432 942 8 51
