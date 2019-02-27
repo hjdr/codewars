4bc358ff7e27d21f3f5391cd91fad768b39d35ee
Harry Riley <harry.jdr@googlemail.com>
2019-02-27 13:55:04 +0000

    finished project

---
+++ mumbling.rb
@@ -0,0 +1,17 @@
+
+mum = "rdogiedgargqE"
+
+def accum(string_to_mumble)
+  counter = 0
+  mumble_string = ""
+	string_to_mumble.downcase!.each_char { |char|
+    counter += 1
+    char *= counter
+    char.sub!(char[0], char[0].upcase)
+    mumble_string += char
+      if counter < string_to_mumble.length
+        mumble_string += "-"
+      end
+    }
+    return mumble_string
+ end
