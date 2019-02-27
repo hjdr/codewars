6b9314de840f9b9450537576426cc09bd40b093c
Harry Riley <harry.jdr@googlemail.com>
2019-02-27 14:51:58 +0000

    Final solution complete

--- get_middle_char.rb
+++ get_middle_char.rb
@@ -1,18 +1,15 @@
 
-test_string = "helloyoui"
+=begin
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+=end
 
 def get_middle(input_str)
   str_length = input_str.length
   str_middle = str_length / 2
-  puts str_length
-  puts str_middle
-  
   if str_length % 2 != 0
     return input_str[str_middle]
   end
+  if str_length % 2 == 0
+    return input_str[str_middle-1..str_middle]
+  end
 end
-
-
-test = get_middle(test_string)
-
-puts test
