3d913291376b8d319a75128a10afad64632489de
Harry Riley <harry.jdr@googlemail.com>
2019-02-28 11:15:59 +0000

    Final solution complete

--- mumbling.rb
+++ mumbling.rb
@@ -1,4 +1,14 @@
 
+=begin
+The examples below show you how to write function accum:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+=end
+
+
 string_to_mumble = 'The quick brown fox jumps over the lazy dog.'
 
 mumble_string = ""
