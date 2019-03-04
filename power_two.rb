63e9c182e485163c882da86973ce362830637385
Harry Riley <harry.jdr@googlemail.com>
2019-03-04 17:49:36 +0000

    Final solution complete

---
+++ power_two.rb
@@ -0,0 +1,16 @@
+=begin
+Complete the function power_of_two/powerOfTwo (or equivalent, depending on your language) that determines if a given non-negative integer is a power of two.
+=end
+
+def power_of_two?(num)
+  output, counter = 0, 0
+  while num > output
+    output = 2**counter
+    counter +=1
+  end
+    num == output
+end
+
+test = power_of_two?(17)
+
+puts test
