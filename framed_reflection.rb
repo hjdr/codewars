5677f53a2804b6df5df0759c5791bfbca4881749
Harry Riley <HarryRiley@applemacs-mbp.home>
2019-03-12 22:32:51 +0000

    Defined function

---
+++ framed_reflection.rb
@@ -0,0 +1,10 @@
+=begin
+You are given a message (text) that you choose to read in a mirror (weirdo). Return what you would see, complete with the mirror frame.
+=end
+
+def mirror(text)
+  text_array = []
+  puts text.split(" ")
+end
+
+test = mirror("hello world")
