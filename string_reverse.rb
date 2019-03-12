1cf6493c2c8137f1799c19d68b3390574ea7e762
Harry Riley <HarryRiley@applemacs-mbp.home>
2019-03-12 22:10:04 +0000

    Added codewars challenge description

--- string_reverse.rb
+++ string_reverse.rb
@@ -1,3 +1,5 @@
+# Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
 def reverse_words(str)
  reversed_string = str.gsub(/([\w.\^$.|?*!@+()]+)/) { |match| match.reverse }
   return reversed_string
