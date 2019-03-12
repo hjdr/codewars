0a673b76fb9a367b8e8f9ebba176d3965087b9b7
Harry Riley <HarryRiley@applemacs-mbp.home>
2019-03-12 22:08:45 +0000

    Refactored

--- string_reverse.rb
+++ string_reverse.rb
@@ -1,25 +1,4 @@
 def reverse_words(str)
-  split_array = str.split(" ")
-  counter = 0
-  reverse_str = ""
-    split_array.each { |word|
-    word.reverse!
-    reverse_str += word.to_s
-    counter += 1
-      if counter < split_array.length
-      reverse_str += " "
-      end
-    }
-  return reverse_str
+ reversed_string = str.gsub(/([\w.\^$.|?*!@+()]+)/) { |match| match.reverse }
+  return reversed_string
 end
-
-output = reverse_words('The quick brown fox jumps over the lazy dog.')
-
-puts output
-
-
-s = 'The quick brown fox jumps over the lazy dog.'
-
-new_string = s.gsub(/([\w.\^$.|?*"!@+()]+)/) { |match| match.reverse }
-
-print new_string
