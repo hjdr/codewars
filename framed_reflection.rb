e428b73fdf12b62538999b7e368dd150d1c240cb
Harry Riley <harry.jdr@googlemail.com>
2019-03-14 19:14:50 +0000

    created vertical frame border

--- framed_reflection.rb
+++ framed_reflection.rb
@@ -2,10 +2,15 @@
 You are given a message (text) that you choose to read in a mirror (weirdo). Return what you would see, complete with the mirror frame.
 =end
 
+
 def mirror(text)
-  text_array = []
-  text_array << text.split(" ").each {|element| element.reverse!}
-  text_array.each {|element| puts element }
+  text_array = text.split(" ").each(&:reverse!)
+  length = text_array.max_by(&:length).length
+  puts "*" * (length + 6)
+  text_array.each { |element| puts "*" + element + "*" }
+  puts "*" * (length + 6)
 end
+
+
 
 test = mirror("hello world how are you")
