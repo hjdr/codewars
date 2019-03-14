416ac200f55f486e41a0104b3185d4930685c237
Harry Riley <harry.jdr@googlemail.com>
2019-03-14 20:08:15 +0000

    Final solution complete

--- framed_reflection.rb
+++ framed_reflection.rb
@@ -2,15 +2,10 @@
 You are given a message (text) that you choose to read in a mirror (weirdo). Return what you would see, complete with the mirror frame.
 =end
 
-
 def mirror(text)
   text_array = text.split(" ").each(&:reverse!)
-  length = text_array.max_by(&:length).length
-  puts "*" * (length + 6)
-  text_array.each { |element| puts "*" + element + "*" }
-  puts "*" * (length + 6)
+  length = text_array.max_by(&:length).length + 4
+  output_string = "*" * length + "\n"
+  text_array.each { |element| output_string += "*" + " " + element + " " * ((length - element.length) - 3) + "*\n" }
+  output_string += "*" * length
 end
-
-
-
-test = mirror("hello world how are you")
