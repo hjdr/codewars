a8a8e396a08c03a8ab608eea66e4b001757b5b32
Harry Riley <harry.jdr@googlemail.com>
2019-03-17 20:34:16 +0000

    Returns when key == value

--- nexus.rb
+++ nexus.rb
@@ -1,14 +1,30 @@
 def nexus(test)
-  print test.sort_by{ |key, value| value }
+  sorted_array = test.sort_by{ |key, value|
+  if key > value
+    key - value
+  else
+    value - key
+  end }
+  print sorted_array
+  array_equals = []
+  sorted_array.each{ |key, value|
+    array_equals << [key, value] if key == value }
+  print array_equals
+  return array_equals[0][0] if array_equals != nil
 end
 
 
 hash_test = {
-  1 => 1,
+  1 => 2,
+  2 => 1,
   3 => 8,
   10 => 16,
   8 => 6,
-  24 => 5
+  24 => 5,
+  30 => 30,
+  14 => 14
 }
 
 test = nexus(hash_test)
+puts "\n"
+puts test
