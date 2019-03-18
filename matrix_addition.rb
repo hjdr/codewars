df8b2c42adb82aa1379a6b1b8da49605f8338f30
Harry Riley <harry.jdr@googlemail.com>
2019-03-18 12:38:26 +0000

    Method created

---
+++ matrix_addition.rb
@@ -0,0 +1,18 @@
+
+
+def matrixAddition(a, b)
+  puts array_length = a.length
+  puts sub_array_length = a.first.length
+  output_array = []
+  array_length_counter = 0
+  sub_array_length_counter = 0
+  while array_length_counter < array_length
+    
+end
+
+
+
+test1 = [[1, 2, 3, 4], [5, 6, 7, 8]]
+test2 = [[1, 2, 3, 4], [5, 6, 7, 8]]
+
+matrixAddition(test1, test2)
