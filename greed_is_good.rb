92e9079f4f0d5c47ab24a944d759fd8bc2921fb3
Harry Riley <harry.jdr@googlemail.com>
2019-03-19 12:05:07 +0000

    created initial function

---
+++ greed_is_good.rb
@@ -0,0 +1,10 @@
+
+def score(dice)
+  c1, c2, c3, c4, c5, c6 = 0, 0, 0, 0, 0, 0
+  arr = [c1, c2, c3, c4, c5, c6]
+  #dice.each{ |dice|
+    #c1 += 1 if dice == 1
+  #}
+end
+
+score([1, 2, 2, 4, 5, 6])
