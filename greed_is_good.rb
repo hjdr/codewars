cb8c2de50587827eb38d5df8d04fea955852f009
Harry Riley <harry.jdr@googlemail.com>
2019-03-19 14:28:21 +0000

    Created count functionality

--- greed_is_good.rb
+++ greed_is_good.rb
@@ -1,10 +1,15 @@
 
 def score(dice)
   c1, c2, c3, c4, c5, c6 = 0, 0, 0, 0, 0, 0
-  arr = [c1, c2, c3, c4, c5, c6]
-  #dice.each{ |dice|
-    #c1 += 1 if dice == 1
-  #}
+  count_arr = [c1, c2, c3, c4, c5, c6]
+  dice.each{ |dice|
+    dice_face, arr_count = 1, 0
+    count_arr.each {|ctr|
+      count_arr[arr_count] += 1 if dice == dice_face
+      puts "counter = #{count_arr[arr_count]} : dice face = #{dice_face}"
+      dice_face += 1
+      arr_count += 1
+    } }
 end
 
-score([1, 2, 2, 4, 5, 6])
+score([2, 2, 2, 4, 5, 6])
