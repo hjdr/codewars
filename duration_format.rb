7ca2490c336347750f6ea4d9cd46605ab98cb61e
Harry Riley <harry.jdr@googlemail.com>
2019-04-14 19:34:23 +0100

    Final solution complete

--- duration_format.rb
+++ duration_format.rb
@@ -1,29 +1,27 @@
-
-
-def duration_format(seconds)
-    sec_total, min_total, hour_total, day_total, year_total = 1, 60, 3600, 86400, 31536000
-    minute_count= seconds / min_total
-    hour_count = seconds / hour_total
-    day_count = seconds / day_total
-    year_count = seconds / year_total
-
-    output = "#{year_count} year, " if day == 1
-    output = "#{year_count} years, " if day > 1
-    day = (seconds % year_total) / day_total
-
-    hour = (seconds % day_total) / hour_total
-    minute = (seconds % hour_total) / min_total
-    seconds = (seconds % min_total) / sec_total
-    print year_count.to_s + " years " + day.to_s + " days " + hour.to_s + " hours " + minute.to_s + " minutes " + seconds.to_s + " seconds " + "\n"
-end
-
-
-duration_format(31649116)
-
-
-=begin
-minute = 60
-hour = 3600
-day = 86400
-year = 31536000
+=begin  
+Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
 =end
+
+def format_duration(seconds)
+    return "now" if seconds == 0
+
+    sec_total, min_total, hour_total, day_total, year_total, output, times, counter = 1, 60, 3600, 86400, 31536000, [], ["year", "day", "hour", "minute", "second"], 0
+    time_totals = [year = seconds / year_total,
+    day = (seconds % year_total) / day_total,
+    hour = (seconds % day_total) / hour_total,
+    minute = (seconds % hour_total) / min_total,
+    second = (seconds % min_total) / sec_total]
+
+    time_totals.each { | time | output << "#{time} #{time == 1 ? times[counter] + "," : times[counter] + "s," }" if time > 0
+    counter +=1 }
+
+    if output.length == 2
+        output[0].chomp!(",")
+        output.insert(1, "and").join(" ").chomp(",")
+    elsif output.length >= 3
+        output[-2].chomp!(",") 
+        output.insert(-2, "and").join(" ").chomp(",")
+    else
+        output.join(" ").chomp(",")
+    end
+end
