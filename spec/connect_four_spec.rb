7e5d5afd2baf6e6fc6d81170571c8a3fb3537d09
hjdr <harry.jdr@gmail.com>
2019-06-16 14:02:10 +0100

    added connect four and rspec folder

---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,10 @@
+
+
+
+# map A-G to 0-6
+# parse each string in input array and remove first two chars
+# add colour to related array
+# loop array and counter ++ if colour:
+#   - sequential in sub array (vertical row)
+#   - sequential in sub array[same index] (horizontal row)
+#   - sequential in sub array[index +1] (diagonal row)
